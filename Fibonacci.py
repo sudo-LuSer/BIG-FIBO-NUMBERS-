b11ab1192203d37895e6caedb4e61850079e6c2f
Nasroallah <65375745+sudo-LuSer@users.noreply.github.com>
2022-08-28 22:17:40 +0100

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,18 @@
+import math
+import numpy 
+import sys
+NMAX = (10**7)+1
+FiboIdentity = [[1,1],
+				[1,0]]
+FIBO = [FiboIdentity]
+def MultiPly2D(A,B):
+	AB = [
+	[(A[0][0]*B[0][0]) + (A[0][1]*B[0][1]) , (A[0][0]*B[0][1]) + (A[0][1]*B[1][1])]
+	,[(A[1][0]*B[0][0]) + (A[1][1]*B[1][0]) , (A[1][0]*B[0][1]) + (A[1][1]*B[1][1])]
+	]
+	return AB 
+def pleinFIBO(FIBO):
+	for i in range(1,NMAX):
+		FIBO.append(MultiPly2D(FIBO[i-1],FiboIdentity))
+pleinFIBO(FIBO)
+print(FIBO[NMAX][1][1])
